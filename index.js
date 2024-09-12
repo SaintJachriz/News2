70a3a371237d82805c947446c287e8ae279e072e
SaintJachriz <mbagwujachi03@gmail.com>
2024-09-12 15:01:38 +0100

    Add files via upload

---
+++ index.js
@@ -0,0 +1,13 @@
+function Show() {
+    const hide = document.querySelector('.hidden');
+    hide.style.display = 'flex';
+}
+
+function Hidden() {
+    const hide = document.querySelector('.hidden');
+    hide.style.display = 'none';
+
+}
+
+
+
